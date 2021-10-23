61f37d91be9c8cc19c7b9c9f4ec3bad732d32add
Yeh-Shao-Chuan <t110598071@ntut.org.tw>
2021-10-24 01:17:28 +0800

    adjust Github.h

--- src/test_github.h
+++ src/test_github.h
@@ -4,6 +4,15 @@ class Github {
     public:
         Github() {num = 0;}
         Github(int k) {num = k;}
+
+        int getNum(int kk) {
+            if (kk == 0) {
+                return kk;
+            }
+            else {
+                return kk-1;
+            }
+        }
     private:
         int num;
 };
